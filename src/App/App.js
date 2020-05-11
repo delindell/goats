9e0b5ae630d45521b6b751ba22e3c9bebf4eb5a9
delindell <daviselindell@gmail.com>
2020-05-11 18:56:26 -0500

    added goats

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
+import goatData from './helpers/data/goatData';
 
 class App extends React.Component {
+  // outside render - anything that modifies state
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    console.error('goats', goats);
+  }
+
   render() {
+    // inside render - anything we need to do to modify UI
     return (
       <div className="App">
-        <h1>Inside App Component</h1>
+        <h1>Goats</h1>
         <button className="btn btn-info">Button things</button>
       </div>
     );
