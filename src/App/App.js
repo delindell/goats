5dd74e9c75f71ae975fd4b147e03f98f14d550c4
delindell <daviselindell@gmail.com>
2020-05-11 19:56:44 -0500

    printing goats

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.scss';
 import goatData from './helpers/data/goatData';
+import GoatCoral from '../components/GoatCoral/GoatCoral';
+
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
   // outside render - anything that modifies state
 
   componentDidMount() {
     const goats = goatData.getGoats();
-    console.error('goats', goats);
+    this.setState({ goats });
   }
 
   render() {
     // inside render - anything we need to do to modify UI
     return (
       <div className="App">
-        <h1>Goats</h1>
-        <button className="btn btn-info">Button things</button>
+        <h1>Goats Yoga LTD</h1>
+        <GoatCoral goats={this.state.goats}/>
       </div>
     );
   }
